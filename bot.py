8d271ca7f0cf6e961e052a894ed3fcbce182fa52
Pedro Henrique Ferreira <pedrohfdias.15122007@gmail.com>
2025-12-16 00:27:17 -0300

    12

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from discord.ui import View, Button, Select, Modal, TextInput
+from dotenv import load_dotenv
+load_dotenv()
 
 # ================= CONFIGURAÇÕES =================
 TOKEN = os.getenv("TOKEN")
